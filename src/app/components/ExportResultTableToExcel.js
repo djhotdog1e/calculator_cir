5a80e5ba6ba75de6577480a9fa260d0115ef63a5
djhotdog1e <iljapetrov111@mail.ru>
2025-06-26 16:29:52 +0300

    export reuslts to excel

---
+++ src/app/components/ExportResultTableToExcel.js
@@ -0,0 +1,19 @@
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
+import { getHeadersExcel } from '../../../utils/getHeadersExcel';
+import { getRowsExcel } from '../../../utils/getRowsExcel';
+
+export function ExportResultTableToExcel({ sections, rows, results, fileName }) {
+  const [header1, header2, header3] = getHeadersExcel(sections);
+  const dataRows = rows.map(row => getRowsExcel(row, sections, results));
+  const worksheetData = [header1, header2, header3, ...dataRows];
+
+  const ws = XLSX.utils.aoa_to_sheet(worksheetData);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws);
+
+  const file = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+
+  const blob = new Blob([file]);
+  saveAs(blob, fileName);
+}
